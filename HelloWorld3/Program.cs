e291f7ab0e673ffaa8c114a9d8b4c2e35ac6ff31
Dylan Adams <113365542+max5max46@users.noreply.github.com>
2022-09-13 09:09:38 -0300

    Initial commit

---
+++ HelloWorld3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int looooop;
+            int hah = 0;
+            for (int looop = 0; looop < 1000000; looop++)
+            {
+
+                Console.WriteLine("Hello World! x" + (looop - 1000));
+
+            }
+            
+        }
+    }
+}
